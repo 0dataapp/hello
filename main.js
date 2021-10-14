ca477f8f902971f71d3d96a072e9df891c10970f
Noel De Martin <noeldemartin@gmail.com>
2021-10-14 08:43:24 +0200

    Extract protocol specific code

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 async function main() {
-    const isLoggedIn = await restoreSession();
+    const user = await restoreSession();
 
-    if (!isLoggedIn)
+    if (!user)
         return;
 
-    loadTasks();
-}
+    document.getElementById('username').innerText = user.name;
+    document.body.classList.add('authenticated');
 
-// ---------------------------- APP ---------------------------------
+    const tasks = await loadTasks();
+
+    for (const task of tasks) {
+        appendTaskItem(task);
+    }
+}
 
 function login() {
     const loginUrl = prompt('Introduce your Solid login url');
@@ -15,15 +20,11 @@ function login() {
     if (!loginUrl)
         return;
 
-    solidClientAuthentication.login({
-        oidcIssuer: loginUrl,
-        redirectUrl: window.location.href,
-        clientName: 'Hello World',
-    });
+    performLogin(loginUrl);
 }
 
 async function logout() {
-    await solidClientAuthentication.logout();
+    await performLogout();
 
     document.body.classList.remove('authenticated');
 }
@@ -34,211 +35,19 @@ async function createTask() {
     if (!description)
         return;
 
-    if (!tasksContainerUrl) {
-        await createSolidContainer(user.storageUrl, 'tasks');
+    const task = await performTaskCreation(description);
 
-        // TODO register in type index.
-
-        tasksContainerUrl = `${user.storageUrl}tasks/`;
-    }
-
-    // TODO escape description string.
-    const documentUrl = await createSolidDocument(tasksContainerUrl, `
-        @prefix schema: <https://schema.org/> .
-
-        <#it>
-            a schema:Action ;
-            schema:actionStatus schema:PotentialActionStatus ;
-            schema:description "${description}" .
-    `);
-    const taskUrl = `${documentUrl}#it`;
-
-    appendTaskItem({ url: taskUrl, description });
+    appendTaskItem(task);
 }
 
-async function updateTask(taskUrl, complete) {
-    const documentUrl = getSolidDocumentUrl(taskUrl);
-
-    await updateSolidDocument(documentUrl, `
-        DELETE DATA {
-            <#it>
-                <https://schema.org/actionStatus>
-                <https://schema.org/${complete ? 'PotentialActionStatus' : 'CompletedActionStatus'}> .
-        } ;
-        INSERT DATA {
-            <#it>
-                <https://schema.org/actionStatus>
-                <https://schema.org/${complete ? 'CompletedActionStatus' : 'PotentialActionStatus'}> .
-        }
-    `);
+async function updateTask(taskUrl, done) {
+    await performTaskUpdate(taskUrl, done);
 }
 
 async function deleteTask(taskUrl, taskElement) {
-    const documentUrl = getSolidDocumentUrl(taskUrl);
-
-    await deleteSolidDocument(taskUrl);
+    await performTaskDeletion(taskUrl);
 
     taskElement.remove();
-}
-
-// ------------------------- INTERNAL -------------------------------
-let user, tasksContainerUrl;
-
-async function readSolidDocument(url) {
-    try {
-        const response = await solidClientAuthentication.fetch(url, { headers: { Accept: 'text/turtle' } });
-
-        if (response.status !== 200)
-            return null;
-
-        const data = await response.text();
-        const parser = new N3.Parser({ baseIRI: url });
-
-        return parser.parse(data);
-    } catch (error) {
-        return null;
-    }
-}
-
-async function createSolidDocument(url, contents) {
-    const response = await solidClientAuthentication.fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'text/turtle' },
-        body: contents,
-    });
-
-    if (response.status !== 201)
-        throw new Error(`Failed creating document at ${url}, returned status ${response.status}`);
-
-    return response.headers.get('Location');
-}
-
-async function updateSolidDocument(url, update) {
-    const response = await solidClientAuthentication.fetch(url, {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/sparql-update' },
-        body: update,
-    });
-
-    if (response.status !== 205)
-        throw new Error(`Failed updating document at ${url}, returned status ${response.status}`);
-}
-
-async function deleteSolidDocument(url) {
-    const response = await solidClientAuthentication.fetch(url, { method: 'DELETE' });
-
-    if (response.status !== 205)
-        throw new Error(`Failed deleting document at ${url}, returned status ${response.status}`);
-}
-
-async function createSolidContainer(url, name) {
-    const response = await solidClientAuthentication.fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/turtle',
-            'Link': '<http://www.w3.org/ns/ldp#BasicContainer>; rel="type"',
-            'Slug': name,
-        },
-    });
-
-    if (response.status !== 201)
-        throw new Error(`Failed creating container at ${url}, returned status ${response.status}`);
-}
-
-function getSolidDocumentUrl(resourceUrl) {
-    const url = new URL(resourceUrl);
-
-    url.hash = '';
-
-    return url.href;
-}
-
-async function fetchUserProfile(webId) {
-    const profileQuads = await readSolidDocument(webId);
-    const nameQuad = profileQuads.find(quad => quad.predicate.value === 'http://xmlns.com/foaf/0.1/name');
-    const storageQuad = profileQuads.find(quad => quad.predicate.value === 'http://www.w3.org/ns/pim/space#storage');
-
-    return {
-        name: nameQuad?.object.value || 'Anonymous',
-        storageUrl: storageQuad?.object.value || await findUserStorage(webId),
-    };
-}
-
-async function findUserStorage(url) {
-    url = url.replace(/#.*$/, '');
-    url = url.endsWith('/') ? url + '../' : url + '/../';
-    url = new URL(url);
-
-    const response = await solidClientAuthentication.fetch(url.href);
-
-    if (response.headers.get('Link')?.includes('<http://www.w3.org/ns/pim/space#Storage>; rel="type"'))
-        return url.href;
-
-    if (url.pathname === '/')
-        return url.href;
-
-    return findUserStorage(url.href)
-}
-
-async function restoreSession() {
-    await solidClientAuthentication.handleIncomingRedirect({ restorePreviousSession: true });
-
-    const session = solidClientAuthentication.getDefaultSession();
-
-    if (!session.info.isLoggedIn)
-        return false;
-
-    user = await fetchUserProfile(session.info.webId);
-
-    document.getElementById('username').innerText = user.name;
-    document.body.classList.add('authenticated');
-
-    return true;
-}
-
-async function loadTasks() {
-    // TODO read from type index.
-    const containerQuads = await readSolidDocument(`${user.storageUrl}tasks/`);
-
-    if (!containerQuads)
-        return;
-
-    tasksContainerUrl = `${user.storageUrl}tasks/`;
-
-    const containmentQuads = containerQuads.filter(quad => quad.predicate.value === 'http://www.w3.org/ns/ldp#contains');
-
-    for (const containmentQuad of containmentQuads) {
-        const documentQuads = await readSolidDocument(containmentQuad.object.value);
-        const typeQuad = documentQuads.find(
-            quad =>
-                quad.predicate.value === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' &&
-                quad.object.value === 'https://schema.org/Action'
-        );
-
-        if (!typeQuad) {
-            // Not a Task, we can ignore this document.
-
-            return;
-        }
-
-        const taskUrl = typeQuad.subject.value;
-        const descriptionQuad = documentQuads.find(
-            quad =>
-                quad.subject.value === taskUrl &&
-                quad.predicate.value === 'https://schema.org/description'
-        );
-        const statusQuad = documentQuads.find(
-            quad =>
-                quad.subject.value === taskUrl &&
-                quad.predicate.value === 'https://schema.org/actionStatus'
-        );
-
-        appendTaskItem({
-            url: taskUrl,
-            description: descriptionQuad?.object.value || '-',
-            completed: statusQuad?.object.value === 'https://schema.org/CompletedActionStatus',
-        });
-    }
 }
 
 function appendTaskItem(task) {
@@ -247,7 +56,7 @@ function appendTaskItem(task) {
     taskItem.innerHTML = `
         <input
             type="checkbox"
-            ${task.completed && 'checked'}
+            ${task.done && 'checked'}
             onchange="updateTask('${task.url}', this.checked)"
         >
         <button
---
+++ solid.js
@@ -0,0 +1,216 @@
+let user, tasksContainerUrl;
+
+async function restoreSession() {
+    await solidClientAuthentication.handleIncomingRedirect({ restorePreviousSession: true });
+
+    const session = solidClientAuthentication.getDefaultSession();
+
+    if (!session.info.isLoggedIn)
+        return false;
+
+    user = await fetchUserProfile(session.info.webId);
+
+    return user;
+}
+
+function performLogin(loginUrl) {
+    solidClientAuthentication.login({
+        oidcIssuer: loginUrl,
+        redirectUrl: window.location.href,
+        clientName: 'Hello World',
+    });
+}
+
+async function performLogout() {
+    await solidClientAuthentication.logout();
+}
+
+async function performTaskCreation(description) {
+    if (!tasksContainerUrl) {
+        await createSolidContainer(user.storageUrl, 'tasks');
+
+        // TODO register in type index.
+
+        tasksContainerUrl = `${user.storageUrl}tasks/`;
+    }
+
+    // TODO escape description string.
+    const documentUrl = await createSolidDocument(tasksContainerUrl, `
+        @prefix schema: <https://schema.org/> .
+
+        <#it>
+            a schema:Action ;
+            schema:actionStatus schema:PotentialActionStatus ;
+            schema:description "${description}" .
+    `);
+    const taskUrl = `${documentUrl}#it`;
+
+    return { url: taskUrl, description };
+}
+
+async function performTaskUpdate(taskUrl, done) {
+    const documentUrl = getSolidDocumentUrl(taskUrl);
+
+    await updateSolidDocument(documentUrl, `
+        DELETE DATA {
+            <#it>
+                <https://schema.org/actionStatus>
+                <https://schema.org/${done ? 'PotentialActionStatus' : 'CompletedActionStatus'}> .
+        } ;
+        INSERT DATA {
+            <#it>
+                <https://schema.org/actionStatus>
+                <https://schema.org/${done ? 'CompletedActionStatus' : 'PotentialActionStatus'}> .
+        }
+    `);
+}
+
+async function performTaskDeletion(taskUrl) {
+    const documentUrl = getSolidDocumentUrl(taskUrl);
+
+    await deleteSolidDocument(taskUrl);
+}
+
+async function loadTasks() {
+    // TODO read from type index.
+    const containerQuads = await readSolidDocument(`${user.storageUrl}tasks/`);
+
+    if (!containerQuads)
+        return [];
+
+    tasksContainerUrl = `${user.storageUrl}tasks/`;
+
+    const tasks = [];
+    const containmentQuads = containerQuads.filter(quad => quad.predicate.value === 'http://www.w3.org/ns/ldp#contains');
+
+    for (const containmentQuad of containmentQuads) {
+        const documentQuads = await readSolidDocument(containmentQuad.object.value);
+        const typeQuad = documentQuads.find(
+            quad =>
+                quad.predicate.value === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' &&
+                quad.object.value === 'https://schema.org/Action'
+        );
+
+        if (!typeQuad) {
+            // Not a Task, we can ignore this document.
+
+            continue;
+        }
+
+        const taskUrl = typeQuad.subject.value;
+        const descriptionQuad = documentQuads.find(
+            quad =>
+                quad.subject.value === taskUrl &&
+                quad.predicate.value === 'https://schema.org/description'
+        );
+        const statusQuad = documentQuads.find(
+            quad =>
+                quad.subject.value === taskUrl &&
+                quad.predicate.value === 'https://schema.org/actionStatus'
+        );
+
+        tasks.push({
+            url: taskUrl,
+            description: descriptionQuad?.object.value || '-',
+            done: statusQuad?.object.value === 'https://schema.org/CompletedActionStatus',
+        });
+    }
+
+    return tasks;
+}
+
+async function readSolidDocument(url) {
+    try {
+        const response = await solidClientAuthentication.fetch(url, { headers: { Accept: 'text/turtle' } });
+
+        if (response.status !== 200)
+            return null;
+
+        const data = await response.text();
+        const parser = new N3.Parser({ baseIRI: url });
+
+        return parser.parse(data);
+    } catch (error) {
+        return null;
+    }
+}
+
+async function createSolidDocument(url, contents) {
+    const response = await solidClientAuthentication.fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/turtle' },
+        body: contents,
+    });
+
+    if (response.status !== 201)
+        throw new Error(`Failed creating document at ${url}, returned status ${response.status}`);
+
+    return response.headers.get('Location');
+}
+
+async function updateSolidDocument(url, update) {
+    const response = await solidClientAuthentication.fetch(url, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/sparql-update' },
+        body: update,
+    });
+
+    if (response.status !== 205)
+        throw new Error(`Failed updating document at ${url}, returned status ${response.status}`);
+}
+
+async function deleteSolidDocument(url) {
+    const response = await solidClientAuthentication.fetch(url, { method: 'DELETE' });
+
+    if (response.status !== 205)
+        throw new Error(`Failed deleting document at ${url}, returned status ${response.status}`);
+}
+
+async function createSolidContainer(url, name) {
+    const response = await solidClientAuthentication.fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/turtle',
+            'Link': '<http://www.w3.org/ns/ldp#BasicContainer>; rel="type"',
+            'Slug': name,
+        },
+    });
+
+    if (response.status !== 201)
+        throw new Error(`Failed creating container at ${url}, returned status ${response.status}`);
+}
+
+function getSolidDocumentUrl(resourceUrl) {
+    const url = new URL(resourceUrl);
+
+    url.hash = '';
+
+    return url.href;
+}
+
+async function fetchUserProfile(webId) {
+    const profileQuads = await readSolidDocument(webId);
+    const nameQuad = profileQuads.find(quad => quad.predicate.value === 'http://xmlns.com/foaf/0.1/name');
+    const storageQuad = profileQuads.find(quad => quad.predicate.value === 'http://www.w3.org/ns/pim/space#storage');
+
+    return {
+        name: nameQuad?.object.value || 'Anonymous',
+        storageUrl: storageQuad?.object.value || await findUserStorage(webId),
+    };
+}
+
+async function findUserStorage(url) {
+    url = url.replace(/#.*$/, '');
+    url = url.endsWith('/') ? url + '../' : url + '/../';
+    url = new URL(url);
+
+    const response = await solidClientAuthentication.fetch(url.href);
+
+    if (response.headers.get('Link')?.includes('<http://www.w3.org/ns/pim/space#Storage>; rel="type"'))
+        return url.href;
+
+    if (url.pathname === '/')
+        return url.href;
+
+    return findUserStorage(url.href)
+}
