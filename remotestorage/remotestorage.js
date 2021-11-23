93b034312da55aea57a437dc38e6ee3d42ea1dfd
Rosano Coutinho <pub@xeari.com>
2021-11-23 16:52:35 -0500

    Implement logout

--- remotestorage/remotestorage.js
+++ remotestorage/remotestorage.js
@@ -57,7 +57,7 @@ function performLogin(storageAddress) {
 }
 
 async function performLogout() {
-    await solidClientAuthentication.logout();
+    return remoteStorage.disconnect();
 }
 
 async function performTaskCreation(description) {
